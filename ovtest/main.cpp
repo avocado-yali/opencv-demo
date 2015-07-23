4b6290bc14eb09323a28d92a33d7029533f50ce4
vitrum <vitrum.cn@gmail.com>
2015-07-23 17:09:50 +0800

    增加了scheme分支，试试ScannerLite的写法

--- ovtest/main.cpp
+++ ovtest/main.cpp
@@ -15,59 +15,17 @@
 using namespace cv;
 using namespace std;
 
-
-//-----------------------------------【全局函数声明部分】--------------------------------------
-//		描述：全局函数声明
-//------------------------------------------------------------------------------------------
-
-
-cv::Point2f center(0,0);
-
-cv::Point2f computeIntersect(cv::Vec4i a,
-                             cv::Vec4i b)
-{
-    int x1 = a[0], y1 = a[1], x2 = a[2], y2 = a[3], x3 = b[0], y3 = b[1], x4 = b[2], y4 = b[3];
-//    float denom;
-    
-    if (float d = ((float)(x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)))
-    {
-        cv::Point2f pt;
-        pt.x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d;
-        pt.y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d;
-        return pt;
-    }
-    else
-        return cv::Point2f(-1, -1);
+/**
+ * Get edges of an image
+ * @param gray - grayscale input image
+ * @param canny - output edge image
+ */
+void getCanny(Mat gray, Mat &canny) {
+    Mat thres;
+    double high_thres = threshold(gray, thres, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU), low_thres = high_thres * 0.5;
+    cv::Canny(gray, canny, low_thres, high_thres);
 }
 
-void sortCorners(std::vector<cv::Point2f>& corners,
-                 cv::Point2f center)
-{
-    std::vector<cv::Point2f> top, bot;
-    
-    for (int i = 0; i < corners.size(); i++)
-    {
-        if (corners[i].y < center.y)
-            top.push_back(corners[i]);
-        else
-            bot.push_back(corners[i]);
-    }
-    corners.clear();
-    
-    if (top.size() == 2 && bot.size() == 2){
-        cv::Point2f tl = top[0].x > top[1].x ? top[1] : top[0];
-        cv::Point2f tr = top[0].x > top[1].x ? top[0] : top[1];
-        cv::Point2f bl = bot[0].x > bot[1].x ? bot[1] : bot[0];
-        cv::Point2f br = bot[0].x > bot[1].x ? bot[0] : bot[1];
-        
-        
-        corners.push_back(tl);
-        corners.push_back(tr);
-        corners.push_back(br);
-        corners.push_back(bl);
-    }
-}
-// 自定义了线的结构
 struct Line {
     Point _p1;
     Point _p2;
@@ -80,169 +38,149 @@ struct Line {
     }
 };
 
-//－导入图片，原始图，缩小图，灰度图，线条图，画框图和效果图
-Mat src = imread("myimg-3.png");
-Mat img_proc ,img_canny, img_line ,img_dis;
-
-const int median_1_max = 100;
-
-int median_1;
-vector<Vec4i> lines;
-vector<Line> horizontals, verticals;
-void myCallBackFunction(int, void*)
-{
-    HoughLinesP(img_canny, lines, 1, CV_PI/90,70,  median_1, 10 );
-    img_line = img_proc.clone();
-    cout<<"new number_1: "<<median_1<<endl;
-    for (int i = 0; i < lines.size(); i++)
-    {
-        Vec4i v = lines[i];
-        line(img_line, Point(v[0], v[1]), Point(v[2], v[3]), CV_RGB(0,255,0));
-        cout<<"line: "<< i+1 <<" of "<< lines.size() <<endl;
-        cout<<v<<endl;
-    }
-
-    
-    imshow("Controls", img_line );
+bool cmp_y(const Line &p1, const Line &p2) {
+    return p1._center.y < p2._center.y;
 }
 
+bool cmp_x(const Line &p1, const Line &p2) {
+    return p1._center.x < p2._center.x;
+}
 
-//-----------------------------------【主函数】--------------------------------------
-//		描述：主函数
-//----------------------------------------------------------------------------------
+/**
+ * Compute intersect point of two lines l1 and l2
+ * @param l1
+ * @param l2
+ * @return Intersect Point
+ */
+Point2f computeIntersect(Line l1, Line l2) {
+    int x1 = l1._p1.x, x2 = l1._p2.x, y1 = l1._p1.y, y2 = l1._p2.y;
+    int x3 = l2._p1.x, x4 = l2._p2.x, y3 = l2._p1.y, y4 = l2._p2.y;
+    if (float d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)) {
+        Point2f pt;
+        pt.x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d;
+        pt.y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d;
+        return pt;
+    }
+    return Point2f(-1, -1);
+}
 
-int main(int argc, char** argv)
-{
+void scan(String file, bool debug = true) {
     
-
-    if (src.empty())
-        return -1;
-
-    
-    int w = src.size().width, h = src.size().height, min_w = 200;
-    double scale = min(20.0, w * 1.0 / min_w);
+    /* get input image */
+    Mat img = imread(file);
+    // resize input image to img_proc to reduce computation
+    Mat img_proc;
+    int w = img.size().width, h = img.size().height, min_w = 200;
+    double scale = min(10.0, w * 1.0 / min_w);
     int w_proc = w * 1.0 / scale, h_proc = h * 1.0 / scale;
-    resize(src, img_proc, Size(w_proc, h_proc));
-//    img_proc = src.clone();
-    img_canny = img_dis = img_line = img_proc.clone();
-    //－缩小一下有助于降低边缘识别线条的难度？总之试试看。
-
-    cout<<"w:"<< src.size().width<< ", w_proc:"<<w_proc << ", scale:"<< scale <<endl;
-
-    cvtColor(img_canny , img_canny, CV_BGR2GRAY);
-    blur(img_canny, img_canny, cv::Size(3, 3));
-    Canny(img_canny, img_canny, 50, 200, 3);
+    resize(img, img_proc, Size(w_proc, h_proc));
+    Mat img_dis = img_proc.clone();
     
-//    imshow("［已经边缘处理过了的图像］", img_canny);
-//    waitKey(0);
-
+    /* get four outline edges of the document */
+    // get edges of the image
+    Mat gray, canny;
+    cvtColor(img_proc, gray, CV_BGR2GRAY);
+    getCanny(gray, canny);
     
+    // extract lines from the edge image
+    vector<Vec4i> lines;
+    vector<Line> horizontals, verticals;
+    HoughLinesP(canny, lines, 1, CV_PI / 180, w_proc / 3, w_proc / 3, 20);
+    for (size_t i = 0; i < lines.size(); i++) {
+        Vec4i v = lines[i];
+        double delta_x = v[0] - v[2], delta_y = v[1] - v[3];
+        Line l(Point(v[0], v[1]), Point(v[2], v[3]));
+        // get horizontal lines and vertical lines respectively
+        if (fabs(delta_x) > fabs(delta_y)) {
+            horizontals.push_back(l);
+        } else {
+            verticals.push_back(l);
+        }
+        // for visualization only
+        if (debug)
+            line(img_proc, Point(v[0], v[1]), Point(v[2], v[3]), Scalar(0, 0, 255), 1, CV_AA);
+    }
     
-//    HoughLinesP(img_canny, lines, 1, CV_PI/90, 60, 50, 30 );
-
-    namedWindow("Controls", 1);
-    createTrackbar("Median 1", "Controls", &median_1, median_1_max, myCallBackFunction );
-
-    myCallBackFunction(median_1, 0);
-    
-    
-//    waitKey(0);
-    
-    
-//    cv::HoughLinesP(bw, lines, 1,CV_PI / 180, w_proc / 3, w_proc / 3, 0);
-    
-    
-    
-    
-
-    
-    
-
-    // 把线打印出来看看-1
-
-
-    
-    
-//    // Expand the lines
-//    for (int i = 0; i < lines.size(); i++)
-//    {
-//        Vec4i v = lines[i];
-//        lines[i][0] = 0;
-//        lines[i][1] = ((float)v[1] - v[3]) / (v[0] - v[2]) * -v[0] + v[1];
-//        lines[i][2] = img_line.cols;
-//        lines[i][3] = ((float)v[1] - v[3]) / (v[0] - v[2]) * (img_line.cols - v[2]) + v[3];
-//    }
-
-    
-    imshow("［已经边缘处理过了的图像］", img_canny);
-    imshow("［把找出来的线画出来］", img_line);
-    waitKey(0);
-    
-    Mat dst = src.clone();
-    
-    vector<Point2f> corners;
-    for (int i = 0; i < lines.size(); i++)
-    {
-        for (int j = i+1; j < lines.size(); j++)
-        {
-            Point2f pt = computeIntersect(lines[i], lines[j]);
-            if (pt.x >= 0 && pt.y >= 0)
-                corners.push_back(pt);
+    // edge cases when not enough lines are detected
+    if (horizontals.size() < 2) {
+        if (horizontals.size() == 0 || horizontals[0]._center.y > h_proc / 2) {
+            horizontals.push_back(Line(Point(0, 0), Point(w_proc - 1, 0)));
+        }
+        if (horizontals.size() == 0 || horizontals[0]._center.y <= h_proc / 2) {
+            horizontals.push_back(Line(Point(0, h_proc - 1), Point(w_proc - 1, h_proc - 1)));
         }
     }
-    
-    vector<Point2f> approx;
-    approxPolyDP(Mat(corners), approx, arcLength(cv::Mat(corners), true) * 0.02, true);
-    cout<<"approx 是个什么鬼？ 为什么要四个？现在有几个："<<approx.size()<<endl;
-    cout<<approx<<endl;
-    
-    
-
-    
-    
-    
-    if (approx.size() != 4)
-    {
-        std::cout << "The object is not quadrilateral!" << std::endl;
-        std::cout << "Approx size is : " <<approx.size()<< std::endl;
-        return -1;
+    if (verticals.size() < 2) {
+        if (verticals.size() == 0 || verticals[0]._center.x > w_proc / 2) {
+            verticals.push_back(Line(Point(0, 0), Point(0, h_proc - 1)));
+        }
+        if (verticals.size() == 0 || verticals[0]._center.x <= w_proc / 2) {
+            verticals.push_back(Line(Point(w_proc - 1, 0), Point(w_proc - 1, h_proc - 1)));
+        }
+    }
+    // sort lines according to their center point
+    sort(horizontals.begin(), horizontals.end(), cmp_y);
+    sort(verticals.begin(), verticals.end(), cmp_x);
+    // for visualization only
+    if (debug) {
+        line(img_proc, horizontals[0]._p1, horizontals[0]._p2, Scalar(0, 255, 0), 2, CV_AA);
+        line(img_proc, horizontals[horizontals.size() - 1]._p1, horizontals[horizontals.size() - 1]._p2, Scalar(0, 255, 0), 2, CV_AA);
+        line(img_proc, verticals[0]._p1, verticals[0]._p2, Scalar(255, 0, 0), 2, CV_AA);
+        line(img_proc, verticals[verticals.size() - 1]._p1, verticals[verticals.size() - 1]._p2, Scalar(255, 0, 0), 2, CV_AA);
     }
     
-    // Get mass center
-    for (int i = 0; i < corners.size(); i++)
-        center += corners[i];
-    center *= (1. / corners.size());
+    /* perspective transformation */
     
-    sortCorners(corners, center);
-    if (corners.size() == 0){
-        std::cout << "The corners were not sorted correctly!" << std::endl;
-//        std::cout << "The corners were not sorted correctly! : " <<corners.size()<< std::endl;
-        return -1;
+    // define the destination image size: A4 - 200 PPI
+    int w_a4 = 1654, h_a4 = 2339;
+    //int w_a4 = 595, h_a4 = 842;
+    Mat dst = Mat::zeros(h_a4, w_a4, CV_8UC3);
+    
+    // corners of destination image with the sequence [tl, tr, bl, br]
+    vector<Point2f> dst_pts, img_pts;
+    dst_pts.push_back(Point(0, 0));
+    dst_pts.push_back(Point(w_a4 - 1, 0));
+    dst_pts.push_back(Point(0, h_a4 - 1));
+    dst_pts.push_back(Point(w_a4 - 1, h_a4 - 1));
+    
+    // corners of source image with the sequence [tl, tr, bl, br]
+    img_pts.push_back(computeIntersect(horizontals[0], verticals[0]));
+    img_pts.push_back(computeIntersect(horizontals[0], verticals[verticals.size() - 1]));
+    img_pts.push_back(computeIntersect(horizontals[horizontals.size() - 1], verticals[0]));
+    img_pts.push_back(computeIntersect(horizontals[horizontals.size() - 1], verticals[verticals.size() - 1]));
+    
+    // convert to original image scale
+    for (size_t i = 0; i < img_pts.size(); i++) {
+        // for visualization only
+        if (debug) {
+            circle(img_proc, img_pts[i], 10, Scalar(255, 255, 0), 3);
+        }
+        img_pts[i].x *= scale;
+        img_pts[i].y *= scale;
     }
+    
+    // get transformation matrix
+    Mat transmtx = getPerspectiveTransform(img_pts, dst_pts);
+    
+    // apply perspective transformation
+    warpPerspective(img, dst, transmtx, dst.size());
+    
+    // save dst img
+    imwrite("dst.jpg", dst);
+    
+    // for visualization only
+    if (debug) {
+        namedWindow("dst", CV_WINDOW_KEEPRATIO);
+        imshow("src", img_dis);
+        imshow("canny", canny);
+        imshow("img_proc", img_proc);
+        imshow("dst", dst);
+        waitKey(0);
+    }
+}
 
-    
-    // Draw corner points
-    cv::circle(img_dis, corners[0], 3, CV_RGB(255,0,0), 2);
-    cv::circle(img_dis, corners[1], 3, CV_RGB(0,255,0), 2);
-    cv::circle(img_dis, corners[2], 3, CV_RGB(0,0,255), 2);
-    cv::circle(img_dis, corners[3], 3, CV_RGB(255,255,255), 2);
-    
-    // Draw mass center
-    cv::circle(img_dis, center, 3, CV_RGB(255,255,0), 2);
-    
-    cv::Mat quad = cv::Mat::zeros(300, 220, CV_8UC3);
-    
-    std::vector<cv::Point2f> quad_pts;
-    quad_pts.push_back(cv::Point2f(0, 0));
-    quad_pts.push_back(cv::Point2f(quad.cols, 0));
-    quad_pts.push_back(cv::Point2f(quad.cols, quad.rows));
-    quad_pts.push_back(cv::Point2f(0, quad.rows));
-    
-    cv::Mat transmtx = cv::getPerspectiveTransform(corners, quad_pts);
-    cv::warpPerspective(src, quad, transmtx, quad.size());
-    
-    cv::imshow("image", img_dis);
-    cv::imshow("quadrilateral", quad);
-    cv::waitKey();
+int main(int argc, char** argv) {
+    string img_path[] = {"images/doc1.jpg", "images/doc2.jpg", "images/doc3.jpg"};
+    scan(img_path[2]);
     return 0;
 }
